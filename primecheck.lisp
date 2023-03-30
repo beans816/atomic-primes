410c3160af43fe63ad7d18ea9cd15f28f0e4b8e2
peach <ginahenstock7@gmail.com>
2023-03-30 11:55:57 +0100

    Create primecheck.lisp

---
+++ primecheck.lisp
@@ -0,0 +1,15 @@
+(defun primep(n)
+  (and (integerp n)
+       (> n 1)
+       ;;fix when learning iteration
+       ;;check out eucilids sequence -> skips 5
+       (if (or(= n 2)
+	      (= n 3)
+	      (= n 5)
+	      (= n 7))
+	   t
+	   (and
+	       (not(zerop(mod n 2)))
+	       (not(zerop(mod n 3)))
+	       (not(zerop(mod n 5)))
+	       (not(zerop(mod n 7)))))))
