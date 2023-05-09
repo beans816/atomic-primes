bdc0e8ab2c0ee0807848ca9056dc1f7fd096cc9d
peach <ginahenstock7@gmail.com>
2023-05-09 13:49:36 +0100

    fixed prime factorisation
    needs to be simplified instead of such a large block of code

--- Goldbach.lisp
+++ Goldbach.lisp
@@ -1,8 +1,12 @@
 ;;so far this only works up to 100, do not change the name of the list
-(defparameter *lpn* '(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97))
+;;lpn stands for lowest prime numbers. This helps with PrimeP in determining the divisibility of a number. 11 is included because it ends in 1.
+(defparameter *lpn* '(2 3 5 7 11))
+
+;;many-primes is intended to be used for cryptography. There are super-computers being used to calculate the next prime number to help with public-keys but it relys on how difficult prime factorisation is.
+(defparameter *many-primes*'(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97))
 
 (defun mod0p (number divisor)
-  "Predicate that returns true if a number is divisible by modulus (0)"
+  "Predicate that returns true if a number is divisible by modulus and returns (0)"
   (zerop (mod number divisor)))
 
 (defun memberp (n list)
@@ -10,13 +14,19 @@
   (and (member n list) t))
 
 (defun is-lpn-p (n)
-  "Predicate that returns T if a number is in the prime numbers list"
+  "Predicate that returns T if a number is in the lowest prime numbers list"
   (memberp n *lpn*))
 
 (defun digit-to-list (n)
   "Turns a number into a list of numbers so large numbers can be easily manipulated"
   (let ((as-string (format nil "~D" n)))
     (mapcar #'parse-integer (mapcar #'string (coerce as-string 'list)))))
+
+(defun how-long-digit (n)
+  "Returns the number of digits in a number"
+  (length (digit-to-list n)))
+
+;;n needs to be smaller then 10 million
 
 (defun primep (n)
   "Predicate returns T if number is a prime."
@@ -27,23 +37,25 @@
 
 (defun Sieve-of-Eratosthenes (number-list)
   "Sieve of Eratosthenes Algorithm removes all numbers in a list that are not prime"
-    (remove-if-not #'primep number-list))
+  (remove-if-not #'primep number-list))
+
+;;this can be fixed
+;;for-every i where mod n i returns true in the list *lpn*
 
 (defun prime-factorisation (n)
   "https://youtu.be/-RhdzNYfF-M"
   (let ((primefactors ()))
     (labels ((pushnumber (n)
-	       (push n primefactors))
-	     (single-factor (n)
-	       (cond ((primep n) (pushnumber n))
-		     ;;for-every i where mod n i returns true in the list *lpn*
-		     ((every (lambda (i)
-			       (mod0p n i))
-			     *lpn*)
-		      ;;almost
-		      (pushnumber i) (single-factor (/ n i))))))
-      (single-factor n)
-      primefactors)))
+               (push n primefactors))
+             (single-factor (n)
+               (cond ((primep n) (pushnumber n))
+                     ((mod0p n 2) (pushnumber 2) (single-factor (/ n 2)))
+                     ((mod0p n 3) (pushnumber 3) (single-factor (/ n 3)))
+                     ((mod0p n 5) (pushnumber 5) (single-factor (/ n 5)))
+                     ((mod0p n 7) (pushnumber 7) (single-factor (/ n 7)))
+                     ((mod0p n 11) (pushnumber 11) (single-factor (/ n 11))))))
+             (single-factor n)
+             primefactors)))
 
 (defun goldbach-conjecture (n lpn)
   "Predicate proof based off of Goldbach's conjecture - will return a list of the SUM of the first two prime numbers. The lowest prime number starts at 2 and increments to the next prime number until it returns that either the difference between the lpn and the even number is also a prime (TRUE) or it will return NIL if Goldbach was wrong. In this way you can define a even number differently as the sum of the two lowest prime numbers."
